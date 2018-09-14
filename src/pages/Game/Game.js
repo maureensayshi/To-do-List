9faa95596b76ecf509692860d6bf4fbb736f806d
maureensayshi <maureen81711@gmail.com>
2018-09-14 17:39:31 +0800

    finish moving cell with rules

--- src/pages/Game/Game.js
+++ src/pages/Game/Game.js
@@ -1,14 +1,25 @@
 import React, {Component} from "react";
 import Form from "./Form.js";
+// import Puzzle from "./Puzzle.js";
 
 export default class Game extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      inputValue:""
+      inputValue:"",
+      cellValue: [1, 2, 3, 4, 5, 6, 7, 8, ""]
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleSwitch = this.handleSwitch.bind(this);
+  }
+
+  componentDidMount(){
+    this.initCellValue();
+  }
+  initCellValue(){
+    let cellValue = this.state.cellValue.sort(() => Math.random() - 0.5);
+    this.setState({cellValue: cellValue});
   }
   handleChange(e) {
     e.preventDefault();
@@ -18,6 +29,37 @@ export default class Game extends Component {
   handleSubmit(e){
     e.preventDefault();
     console.log("handle submit");    
+  }
+  replaceNum(index, value){
+    let cellValue = this.state.cellValue;
+    cellValue[index] = value;
+    this.setState({cellValue: cellValue});
+  }
+  handleSwitch(i){
+    console.log("click index : " + i);
+    let curNum = this.state.cellValue[i];
+    let leftNum = this.state.cellValue[i -1];
+    let rightNum = this.state.cellValue[i + 1];
+    let topNum = this.state.cellValue[i - 3];
+    let bottomNum = this.state.cellValue[i + 3];
+
+    console.log(curNum, leftNum, rightNum, topNum, bottomNum);
+    if(leftNum === "" && i % 3){
+      this.replaceNum(i - 1, curNum);
+      this.replaceNum(i, "");
+    } 
+    else if(rightNum === "" && 2 !== i % 3){
+      this.replaceNum(i + 1, curNum);
+      this.replaceNum(i, "");
+    }
+    else if(topNum === ""){
+      this.replaceNum(i - 3, curNum);
+      this.replaceNum(i, "");
+    }
+    else if(bottomNum === ""){
+      this.replaceNum(i + 3, curNum);
+      this.replaceNum(i, "");
+    }    
   }
   render() {
     return (
@@ -31,6 +73,11 @@ export default class Game extends Component {
           <span className="count">Step Count : </span>
           <span>0</span>
         </div>
+        <ul className="board">
+          {this.state.cellValue.map((item, i) => {
+            return <li className="cell" key={i} onClick={() => this.handleSwitch(i)}>{item}</li>;
+          })}
+        </ul>
       </div>
     );
   }
